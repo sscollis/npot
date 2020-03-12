90b4276a955d884306aa8a9fcd7011e8d028fd1f
Scott Collis <sscollis@gmail.com>
2020-03-12 07:23:14 -0600

    added and updated

--- test/naca/README.md
+++ test/naca/README.md
@@ -1,1 +1,16 @@
-This metric file is in JI ordering!
+## NACA Airfoil
+
+Note:  This metric file is in JI ordering!
+
+### Sample Results
+
+#### Contours of streamwise velocity
+
+![Streamwise velocity](https://github.com/sscollis/npot/blob/master/test/naca/u.png)
+
+#### Computational Mesh
+
+![Mesh](https://github.com/sscollis/npot/blob/master/test/naca/mesh.png)
+
+S. Scott Collis\
+Thu Mar 12 07:22:34 MDT 2020
--- test/naca/run.sh
+++ test/naca/run.sh
@@ -4,6 +4,6 @@ NPOT=../../src/npot
 env GFORTRAN_CONVERT_UNIT='swap' $NPOT -wc < npot.inp | tee npot.log
 env GFORTRAN_CONVERT_UNIT='swap' $NPOT -wc < res1.inp | tee npot.log
 env GFORTRAN_CONVERT_UNIT='swap' $NPOT -wc < res2.inp | tee npot.log
-env GFORTRAN_CONVERT_UNIT='swap' $NPOT -wc < res1.inp | tee npot.log
-env GFORTRAN_CONVERT_UNIT='swap' $NPOT -wc < res2.inp | tee npot.log
+#env GFORTRAN_CONVERT_UNIT='swap' $NPOT -wc < res1.inp | tee npot.log
+#env GFORTRAN_CONVERT_UNIT='swap' $NPOT -wc < res2.inp | tee npot.log
 exit 0
