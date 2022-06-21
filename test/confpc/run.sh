1ba603682156e5fb4dbfdb0672f2228862d3fb17
S. Scott Collis <sscollis@me.com>
2022-06-21 09:02:09 -0600

    Added

--- test/confpc/run.sh
+++ test/confpc/run.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 LNS3D_DIR=../../../lns3d
 NPOT=../../src/npot
-$LNS3D_DIR/mesh/confpc $2 < confpc.inp.$1 > confpc.log
-$NPOT < npot.inp | tee npot.log
-$NPOT < restart-1.inp | tee npot.log
-$NPOT < restart-2.inp | tee npot.log
-$NPOT < restart-3.inp | tee npot.log
-$NPOT < restart-4.inp | tee npot.log
-$NPOT < restart-5.inp | tee npot.log
-$NPOT < restart-4.inp | tee npot.log
-$NPOT < restart-3.inp | tee npot.log
+if [[ $# -lt 1 ]]; then
+  echo "Usage:  run.sh case args" 
+  exit 2
+else
+  case=$1
+  shift
+fi
+echo "Running case $case with args: $@"
+#set -x
+$LNS3D_DIR/mesh/confpc $@ < confpc.inp.$case > confpc.log
+$NPOT < npot.inp.0 | tee npot.log
+$NPOT < npot.inp.1 | tee npot.log
+$NPOT < npot.inp.2 | tee npot.log
+$NPOT < npot.inp.3 | tee npot.log
+$NPOT < npot.inp.4 | tee npot.log
+$NPOT < npot.inp.5 | tee npot.log
+$NPOT < npot.inp.4 | tee npot.log
+$NPOT < npot.inp.3 | tee npot.log
 $LNS3D_DIR/util/npost lns.dat
-\cp wall.dat wall.dat.$1
+\cp wall.dat wall.dat.$case
+\cp lns.dat.q lns.q.$case
 exit 0
