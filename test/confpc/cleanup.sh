4f79ea6d9b30a01ccbdccf95cec35e08e4b86c03
S. Scott Collis <sscollis@me.com>
2022-06-21 08:56:28 -0600

    Update

--- test/confpc/cleanup.sh
+++ test/confpc/cleanup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rm -f comp.xyz fort.* *.dat metric.ji *.log *.pot output.*
+\rm -f comp.xyz fort.* *.dat metric.ji *.log *.pot output.* grid.xyz lns.dat.q\
+wall.dat.?
--- test/confpc/run.sh
+++ test/confpc/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 LNS3D_DIR=../../../lns3d
 NPOT=../../src/npot
-$LNS3D_DIR/mesh/confpc < confpc.inp > confpc.log
-$NPOT -ms < npot.inp | tee npot.log
-$NPOT -ms < restart-1.inp | tee npot.log
-$NPOT -ms < restart-2.inp | tee npot.log
-$NPOT -ms < restart-3.inp | tee npot.log
-$NPOT -ms < restart-4.inp | tee npot.log
-$NPOT -ms < restart-5.inp | tee npot.log
-$NPOT -ms < restart-4.inp | tee npot.log
-$NPOT -ms < restart-3.inp | tee npot.log
+$LNS3D_DIR/mesh/confpc $2 < confpc.inp.$1 > confpc.log
+$NPOT < npot.inp | tee npot.log
+$NPOT < restart-1.inp | tee npot.log
+$NPOT < restart-2.inp | tee npot.log
+$NPOT < restart-3.inp | tee npot.log
+$NPOT < restart-4.inp | tee npot.log
+$NPOT < restart-5.inp | tee npot.log
+$NPOT < restart-4.inp | tee npot.log
+$NPOT < restart-3.inp | tee npot.log
+$LNS3D_DIR/util/npost lns.dat
+\cp wall.dat wall.dat.$1
 exit 0
